4909b0d5e6d098bfb8f3a7b23cb4bac628d380db
Riyad-0 <riyad.rahman73@login.cuny.edu>
2026-06-16 15:15:45 -0400

    c

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,11 @@
-import './style.css'
+import './style.css';
 import { initialDrawState, requestDraw } from './draw';
 import initCanvas from './initCanvas';
 import handleWindowResize from './handleCanvasResize';
 import type { State } from './state';
 import { screenHeight, screenWidth } from './screen';
 import { dt } from './consts';
+import { handleTouchCancel, handleTouchEnd, handleTouchMove, handleTouchStart, initialTouchState, updateTouchState } from './touchEvent';
 
 function init() {
   const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
@@ -14,19 +15,46 @@ function init() {
 
   let state: State = {
     drawState: initialDrawState,
+    touchState: initialTouchState
   };
 
   function onWindowResize() {
     handleWindowResize(state, canvas, ctx);
   }
 
+  function onTouchStart(e: TouchEvent) {
+    handleTouchStart(state.touchState.touchEvents, e);
+  }
+
+  function onTouchMove(e: TouchEvent) {
+    handleTouchMove(state.touchState.touchEvents, e);
+  }
+
+  function onTouchEnd(e: TouchEvent) {
+    handleTouchEnd(state.touchState.touchEvents, e);
+  }
+
+  function onTouchCancel(e: TouchEvent) {
+    handleTouchCancel(state.touchState.touchEvents, e);
+  }
+
   window.addEventListener("resize", onWindowResize);
+
+  // Need passive: false for some Chrome-specific thing?
+  // https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent
+  window.addEventListener('touchstart', onTouchStart, {passive: false})
+  window.addEventListener('touchmove', onTouchMove, {passive: false})
+  window.addEventListener('touchend', onTouchEnd, {passive: false})
+  window.addEventListener('touchcancel', onTouchCancel, {passive: false})
 
   // console.time();
   function tick() {
     // console.timeLog();
     // console.timeEnd();
     // console.time();
+    console.log(state.touchState.touchEvents);
+
+    updateTouchState(state.touchState);
 
     requestDraw(state, canvas, ctx);
   }
--- client/src/state.ts
+++ client/src/state.ts
@@ -1,7 +1,9 @@
 import type { DrawState } from "./draw";
+import type { TouchState } from "./touchEvent";
 
 interface State {
   drawState: DrawState,
+  touchState: TouchState
 }
 
 export type { State };
